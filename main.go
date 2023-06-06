c5de2cf04d129a4fae12b145f8bf1e431dd6aa1d
Juan.Ferreira <juan.ferreira@impar.com.br>
2023-06-06 12:48:19 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"github.com/JuanCampbsi/go-opportunities/config"
 	"github.com/JuanCampbsi/go-opportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("config initialization error: %v", err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 }
