27428330de08afca40dd66590aba6a1795f60e2d
Juan.Ferreira <juan.ferreira@impar.com.br>
2023-06-09 18:33:15 -0300

    feat: show opening search

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/JuanCampbsi/go-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+
+	//Find Opening
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
