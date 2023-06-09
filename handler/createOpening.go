9272a038e74459ab1c8ed4a124ff6299c7b62a92
Juan.Ferreira <juan.ferreira@impar.com.br>
2023-06-09 18:04:34 -0300

    feat: add create opening

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/JuanCampbsi/go-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,11 +14,24 @@ func CreateOpeningHandler(ctx *gin.Context) {
 
 	if err := request.Validate(); err != nil {
 		logger.ErrorF("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	if err := db.Create(&request).Error; err != nil {
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
 		logger.ErrorF("error creating opening: %+v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
 		return
 	}
+
+	sendSuccess(ctx, "create-opening", opening)
 }
